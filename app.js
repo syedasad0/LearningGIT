441b13d72e9304ec6a688c0d1606cd8fa553e370
Syed Asad <syedasad@ip-192-168-30-10.ap-south-1.compute.internal>
2020-06-20 22:03:32 +0530

    First Commit on Master Branch

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    console.log("Welcome to GIT");
+});
+
+ 
+app.listen(3000);
+console.log("Server Listening at 3000");
